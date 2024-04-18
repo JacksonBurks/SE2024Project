a3e57cc7a9d89e6da6a0d7069331cfa878571ae9
ardleypacheco <89606723+ardleypacheco@users.noreply.github.com>
2024-04-18 16:35:49 -0500

    Update GameServer.java

--- WheelOfFortune/wheelOfFortune/GameServer.java
+++ WheelOfFortune/wheelOfFortune/GameServer.java
@@ -1,38 +1,33 @@
 package wheelOfFortune;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Random;
-
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
-public class GameServer extends AbstractServer
-{
-	// Data fields for this chat server.
-	private JTextArea log;
-	private JLabel status;
-	private boolean running = false;
-	private Database db;
-	private Object newResult;
-	private Object createResult;
-	private Object readyResult;
-	private final int MIN_PLAYERS = 2;
-	private final int MAX_PLAYERS = 4;
-	private final int MIN_POINTS = 300;
-	private final int MAX_POINTS = 1000;
-	private final int INCREMENT = 50;
-	private int playersConnected = 0;
+public class GameServer extends AbstractServer {
+    private JTextArea log;
+    private JLabel status;
+    private boolean running = false;
+    private Database db;
+    private int playersConnected = 0;
+    private ArrayList<Player> players = new ArrayList<>();
+    private static final int MIN_PLAYERS = 2;
+    private static final int MAX_PLAYERS = 4;
+    private static final int MIN_POINTS = 300;
+    private static final int MAX_POINTS = 1000;
+    private static final int INCREMENT = 50;
 
-	// Constructor for initializing the server with default settings.
-	public GameServer()
-	{
-		super(12345);
-		this.setTimeout(500);
-		db = new Database();
-	}
+    public GameServer() {
+        super(12345);
+        this.setTimeout(500);
+        db = new Database();
+    }
 
 	// Getter that returns whether the server is currently running.
 	public boolean isRunning()
@@ -54,176 +49,126 @@ public class GameServer extends AbstractServer
 		this.status = status;
 	}
 
-	// When the server starts, update the GUI.
-	public void serverStarted()
-	{
-		running = true;
-		status.setText("Listening");
-		status.setForeground(Color.GREEN);
-		log.append("Server started\n");
-	}
+    @Override
+    protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+        if (msg instanceof LoginData) {
+            handleLogin((LoginData) msg, client);
+        } else if (msg instanceof CreateAccountData) {
+            handleCreateAccount((CreateAccountData) msg, client);
+        } else if (msg instanceof NewGameData) {
+            handleNewGame((NewGameData) msg, client);
+        }
+    }
 
-	// When the server stops listening, update the GUI.
-	public void serverStopped()
-	{
-		status.setText("Stopped");
-		status.setForeground(Color.RED);
-		log.append("Server stopped accepting new clients - press Listen to start accepting new clients\n");
-	}
+    private void handleLogin(LoginData data, ConnectionToClient client) {
+        try {
+            if (db.verifyAccount(data.getUsername(), data.getPassword())) {
+                client.sendToClient("LoginSuccessful");
+                log.append("Player " + client.getId() + " successfully logged in as " + data.getUsername() + "\n");
+                players.add(new Player(data.getUsername(), data.getPassword(), 0));
+            } else {
+                client.sendToClient(new Error("The username and password are incorrect.", "Login"));
+                log.append("Player " + client.getId() + " failed to log in\n");
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	// When the server closes completely, update the GUI.
-	public void serverClosed()
-	{
-		running = false;
-		status.setText("Close");
-		status.setForeground(Color.RED);
-		log.append("Server and all current clients are closed - press Listen to restart\n");
-	}
+    private void handleCreateAccount(CreateAccountData data, ConnectionToClient client) {
+        try {
+            if (!db.accountExists(data.getUsername(), data.getPassword())) {
+                client.sendToClient("CreateAccountSuccessful");
+                log.append("Player " + client.getId() + " created a new account called " + data.getUsername() + "\n");
+                players.add(new Player(data.getUsername(), data.getPassword(), 0));
+            } else {
+                client.sendToClient(new Error("The username is already in use.", "CreateAccount"));
+                log.append("Player " + client.getId() + " failed to create a new account\n");
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	// When a client connects or disconnects, display a message in the log.
-	public void clientConnected(ConnectionToClient client)
-	{
-		log.append("Player " + client.getId() + " connected\n");
-		playersConnected++;
-	}
+    private void handleNewGame(NewGameData data, ConnectionToClient client) {
+        if (data.isReady()) {
+            if (playersConnected >= MIN_PLAYERS && playersConnected <= MAX_PLAYERS) {
+                try {
+                    client.sendToClient("PlayerReady");
+                    log.append("Player " + client.getId() + " is ready\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    client.sendToClient(new Error("Need more players to connect.", "NotReady"));
+                    log.append("Need more players to start the game\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	// When a message is received from a client, handle it.
-	public void handleMessageFromClient(Object arg0, ConnectionToClient arg1)
-	{
-		// If we received LoginData, verify the account information.
-		if (arg0 instanceof LoginData)
-		{
-			// Check the username and password with the database.
-			LoginData data = (LoginData)arg0;
-			//Object result;
-			try {
-				if (db.verifyAccount(data.getUsername(), data.getPassword()))
-				{
-					newResult = "LoginSuccessful";
-					log.append("Player " + arg1.getId() + " successfully logged in as " + data.getUsername() + "\n");
-				}
-				else
-				{
-					newResult = new Error("The username and password are incorrect.", "Login");
-					log.append("Player " + arg1.getId() + " failed to log in\n");
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+    @Override
+    protected synchronized void clientConnected(ConnectionToClient client) {
+        playersConnected++;
+        log.append("Player " + client.getId() + " connected\n");
+    }
 
-			// Send the result to the client.
-			try
-			{
-				arg1.sendToClient(newResult);
-			}
-			catch (IOException e)
-			{
-				return;
-			}
-		}
+    @Override
+    protected synchronized void clientDisconnected(ConnectionToClient client) {
+        playersConnected--;
+        log.append("Player " + client.getId() + " disconnected\n");
+    }
 
-		// If we received CreateAccountData, create a new account.
-		else if (arg0 instanceof CreateAccountData)
-		{
-			// Try to create the account.
-			CreateAccountData data = (CreateAccountData)arg0;
-			try {
-				if (!db.accountExists(data.getUsername(), data.getPassword()))
-				{
-					createResult = "CreateAccountSuccessful";
-					log.append("Player " + arg1.getId() + " created a new account called " + data.getUsername() + "\n");
-				}
-				else
-				{
-					createResult = new Error("The username is already in use.", "CreateAccount");
-					log.append("Player " + arg1.getId() + " failed to create a new account\n");
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+    public void serverStarted() {
+        running = true;
+        status.setText("Listening");
+        status.setForeground(Color.GREEN);
+        log.append("Server started\n");
+    }
 
-			// Send the result to the client.
-			try
-			{
-				arg1.sendToClient(createResult);
-			}
-			catch (IOException e)
-			{
-				return;
-			}
-		}
-		else if (arg0 instanceof NewGameData)
-		{
-			// Try to create the new Game.
-			NewGameData data = (NewGameData)arg0;
+    public void serverStopped() {
+        status.setText("Stopped");
+        status.setForeground(Color.RED);
+        log.append("Server stopped accepting new clients - press Listen to start accepting new clients\n");
+    }
 
-			if (data.isReady())
-			{
-				if (playersConnected >= MIN_PLAYERS && playersConnected <= MAX_PLAYERS) {
-					readyResult = "PlayerReady";
-					log.append("Player " + arg1.getId() + " is ready\n");
-				}
-				else
-				{
-					readyResult = new Error("Need more players to connect.", "NotReady");
-					log.append("Need more players to start the game\n");
-				}
-			}
-			// Send the result to the client.
-			try
-			{
-				arg1.sendToClient(readyResult);
-			}
-			catch (IOException e)
-			{
-				return;
-			}
-		}
-	}
-	
+    public void serverClosed() {
+        running = false;
+        status.setText("Close");
+        status.setForeground(Color.RED);
+        log.append("Server and all current clients are closed - press Listen to restart\n");
+    }
+
+    public void listeningException(Throwable exception) {
+        running = false;
+        status.setText("Exception occurred while listening");
+        status.setForeground(Color.RED);
+        log.append("Listening exception: " + exception.getMessage() + "\n");
+        log.append("Press Listen to restart server\n");
+    }
+
     public int pointsPossible() {
         Random random = new Random();
-        int points = MIN_POINTS + random.nextInt((MAX_POINTS - MIN_POINTS) / INCREMENT) * INCREMENT;
-        return points;
+        return MIN_POINTS + random.nextInt((MAX_POINTS - MIN_POINTS) / INCREMENT) * INCREMENT;
     }
-    
+
     public String specialSpun() {
         Random random = new Random();
-        // Generate a random number between 0 and 1
-        int randomNumber = random.nextInt(2);
-        // If the random number is 0, return "Lose Turn"
-        if (randomNumber == 0) {
-            return "Lose Turn";
-        } else { // Otherwise, return "Bankrupt"
-            return "Bankrupt";
-        }
+        return random.nextInt(2) == 0 ? "Lose Turn" : "Bankrupt";
     }
-    
+
     public void callMethodBasedOnProbability() {
         Random random = new Random();
-        int randomNumber = random.nextInt(100); // Generate a random number between 0 and 99
-        
-        if (randomNumber < 75) { // 75% probability for pointsPossible() method
+        int randomNumber = random.nextInt(100);
+        if (randomNumber < 75) {
             int points = pointsPossible();
             System.out.println("Points possible: " + points);
-        } else if (randomNumber < 90) { // 15% probability for specialSpun() method
+        } else if (randomNumber < 90) {
             String special = specialSpun();
             System.out.println("Special spun: " + special);
-        } else {
-            // Do nothing for the remaining 10% probability
         }
     }
-
-
-	// Method that handles listening exceptions by displaying exception information.
-	public void listeningException(Throwable exception) 
-	{
-		running = false;
-		status.setText("Exception occurred while listening");
-		status.setForeground(Color.RED);
-		log.append("Listening exception: " + exception.getMessage() + "\n");
-		log.append("Press Listen to restart server\n");
-	}
 }
